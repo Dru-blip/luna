15ddbbf5d44255119b05cf8dacc97823c5ef9de9
Dru-blip <62353412+Dru-blip@users.noreply.github.com>
2025-11-03 23:03:11 +0530

    Implemented object property access via subscript operator.
    - The VM now correctly handles loading and storing object properties using string keys for
    `OPCODE_LOAD_SUBSCR` and `OPCODE_STORE_SUBSCR`. - Enhanced error handling for subscript operations,
    providing specific messages for invalid array indices, non-string object accessors, and attempts to
    access non-existent object properties. - Store the result of module execution in `module->exported`.

--- src/bytecode/interpreter.c
+++ src/bytecode/interpreter.c
@@ -185,9 +185,11 @@ struct lu_value lu_run_program(struct lu_istate* state, const char* filepath) {
         return lu_value_undefined();
     }
     print_executable(executable);
+
     lu_obj_set(state->module_cache, module->name, lu_value_object(module));
     struct lu_value result = lu_value_none();
     result = lu_run_executable(state, executable);
+    module->exported = result;
     // print_value(result);
     state->running_module = prev_module;
 
