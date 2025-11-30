c78e68393af24eb8ee1b2005387d1f31bdfe7c42
Dru-blip <62353412+Dru-blip@users.noreply.github.com>
2025-11-30 22:55:27 +0530

    Introduce Tokenizer component.
    This commit adds the core `Tokenizer` module, responsible for breaking source code into a stream of
    tokens. It supports basic arithmetic operators, integers, identifiers, and a placeholder for
    keywords.

---
+++ src/core/Tokenizer.zig
@@ -0,0 +1,152 @@
+const std = @import("std");
+
+const Token = struct {
+    tag: Tag,
+    loc: Loc,
+
+    pub const Loc = struct {
+        start: usize,
+        end: usize,
+    };
+
+    const Tag = enum {
+        invalid,
+        plus,
+        minus,
+        asterisk,
+        slash,
+        l_paren,
+        r_paren,
+        l_brace,
+        r_brace,
+
+        int,
+        identifier,
+        eof,
+    };
+
+    pub const keywords = std.StaticStringMap(Tag).initComptime(.{
+        .{ "return", .keyword_return },
+    });
+
+    pub fn getKeyword(bytes: []const u8) ?Tag {
+        return keywords.get(bytes);
+    }
+};
+
+buffer: [:0]const u8,
+index: usize,
+
+const Tokenizer = @This();
+
+pub fn dump(self: *Tokenizer, token: *const Token) void {
+    std.debug.print("{s} \"{s}\"\n", .{ @tagName(token.tag), self.buffer[token.loc.start..token.loc.end] });
+}
+
+pub fn init(buffer: [:0]const u8) Tokenizer {
+    return .{
+        .buffer = buffer,
+        .index = if (std.mem.startsWith(u8, buffer, "\xEF\xBB\xBF")) 3 else 0,
+    };
+}
+
+const State = enum {
+    start,
+    identifier,
+    plus,
+    minus,
+    asterisk,
+    slash,
+    int,
+    invalid,
+};
+
+pub fn next(self: *Tokenizer) Token {
+    var result: Token = .{
+        .tag = undefined,
+        .loc = .{
+            .start = self.index,
+            .end = undefined,
+        },
+    };
+
+    state: switch (State.start) {
+        .start => switch (self.buffer[self.index]) {
+            0 => {
+                if (self.index == self.buffer.len) {
+                    return .{
+                        .tag = .eof,
+                        .loc = .{
+                            .start = self.index,
+                            .end = self.index,
+                        },
+                    };
+                } else {
+                    continue :state .invalid;
+                }
+            },
+            '+' => continue :state .plus,
+            '-' => continue :state .minus,
+            '*' => continue :state .asterisk,
+            '/' => continue :state .slash,
+            '0'...'9' => {
+                result.tag = .int;
+                continue :state .int;
+            },
+            'a'...'z', 'A'...'Z', '_' => {
+                result.tag = .identifier;
+                continue :state .identifier;
+            },
+            else => continue :state .invalid,
+        },
+        .plus => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                else => result.tag = .plus,
+            }
+        },
+        .minus => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                else => result.tag = .minus,
+            }
+        },
+        .asterisk => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                else => result.tag = .asterisk,
+            }
+        },
+        .slash => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                else => result.tag = .slash,
+            }
+        },
+        .identifier => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                'a'...'z', 'A'...'Z', '_' => continue :state .identifier,
+                else => {
+                    const ident = self.buffer[result.loc.start..self.index];
+                    if (Token.getKeyword(ident)) |tag| {
+                        result.tag = tag;
+                    }
+                },
+            }
+        },
+        .int => {
+            self.index += 1;
+            switch (self.buffer[self.index]) {
+                '0'...'9' => continue :state .int,
+                else => {},
+            }
+        },
+        .invalid => {
+            result.tag = .invalid;
+        },
+    }
+
+    result.loc.end = self.index;
+    return result;
+}
