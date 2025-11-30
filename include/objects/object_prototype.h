c93ffdb2f783b3410c2bf130b6d09d19f7a02482
Dru-blip <62353412+Dru-blip@users.noreply.github.com>
2025-11-30 21:02:04 +0530

    Migrating codebase to Zig
    This commit replaces the entire C implementation with a new Zig-based codebase.

--- include/objects/object_prototype.h
+++
@@ -1,7 +0,0 @@
-#include "luna.h"
-
-LU_NATIVE_FN(Object_to_string);
-LU_NATIVE_FN(Object_has_property);
-LU_NATIVE_FN(Object_get_proto);
-
-struct lu_object* lu_object_prototype_new(struct lu_istate* state);
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include "bytecode/interpreter.h"
-
-int main(int argc, char* argv[]) {
-    struct lu_istate* state = lu_istate_new();
-    lu_istate_process_init(state, argc, argv);
-    lu_run_program(state, argv[1]);
-    lu_istate_destroy(state);
-    return 0;
-}
